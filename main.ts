1336815631ed7a7e95aaf110d15789caedb5ea10
Nebilam <49345234+Nebilam@users.noreply.github.com>
2019-04-06 19:29:13 +0200

    eerste commit

--- main.ts
+++ main.ts
@@ -1,7 +1,45 @@
-basic.showLeds(`
-    . . . . .
-    . # . # .
-    . . . . .
-    # . . . #
-    . # # # .
-    `);
+input.onButtonPressed(Button.A, function () {
+    devices.tellCameraTo(MesCameraEvent.StopPhotoMode)
+    basic.showLeds(`
+        # # # # #
+        # . . . #
+        # . . . #
+        # . . . #
+        # # # # #
+        `)
+    basic.pause(5000)
+    basic.showLeds(`
+        . . . . .
+        . # # # .
+        . # . # .
+        . # # # .
+        . . . . .
+        `)
+    devices.tellCameraTo(MesCameraEvent.TakePhoto)
+    basic.pause(1000)
+    basic.clearScreen()
+})
+input.onButtonPressed(Button.B, function () {
+    devices.tellCameraTo(MesCameraEvent.LaunchVideoMode)
+    basic.showLeds(`
+        # # # # #
+        # . . . #
+        # . . . #
+        # . . . #
+        # # # # #
+        `)
+    basic.pause(5000)
+    basic.showLeds(`
+        . . . . .
+        . # # # .
+        . # . # .
+        . # # # .
+        . . . . .
+        `)
+    devices.tellCameraTo(MesCameraEvent.StartVideoCapture)
+})
+input.onButtonPressed(Button.AB, function () {
+    devices.tellCameraTo(MesCameraEvent.StopVideoMode)
+    basic.clearScreen()
+})
+ 
